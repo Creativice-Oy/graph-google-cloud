65cc98c46e706171792798257611a15d7aa2e3d0
Paul Ilaga <pgilaga@gmail.com>
2021-08-12 18:01:13 +0800

    Build cluster and compute image relationships

--- src/steps/dataproc/constants.ts
+++ src/steps/dataproc/constants.ts
@@ -4,3 +4,8 @@ export const ENTITY_CLASS_DATAPROC_CLUSTER = 'Cluster';
 export const ENTITY_TYPE_DATAPROC_CLUSTER = 'google_dataproc_cluster';
 export const RELATIONSHIP_TYPE_DATAPROC_CLUSTER_USES_KMS_CRYPTO_KEY =
   'google_dataproc_cluster_uses_kms_crypto_key';
+
+export const STEP_CREATE_CLUSTER_IMAGE_RELATIONSHIPS =
+  'create-cluster-image-relationships';
+export const RELATIONSHIP_TYPE_DATAPROC_CLUSTER_USES_COMPUTE_IMAGE =
+  'google_dataproc_cluster_uses_compute_image';
