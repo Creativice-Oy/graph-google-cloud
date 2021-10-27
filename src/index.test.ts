34d8fde6e4b161da02c50d7a3900ca5814702f70
Paul Ilaga <pgilaga@gmail.com>
2021-10-27 20:59:50 +0800

    Fix rebase issues

--- src/index.test.ts
+++ src/index.test.ts
@@ -449,9 +449,6 @@ describe('#getStepStartStates success', () => {
       [STEP_AUDIT_CONFIG_IAM_POLICY]: {
         disabled: true,
       },
-      [STEP_AUDIT_CONFIG_IAM_POLICY]: {
-        disabled: true,
-      },
       [STEP_COMPUTE_DISKS]: {
         disabled: false,
       },
